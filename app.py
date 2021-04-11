d1e14f9d3f563f0ae6809f12dd77b99c1b6456c2
Jeff Hall <jeff.leon.hall@gmail.com>
2021-04-11 13:47:25 -0700

    the register route can now create users, the register html form now accepts POST requests, and created a new secret page

--- app.py
+++ app.py
@@ -20,8 +20,26 @@ def redirect_route():
     """Redirect user to /register."""
     return redirect('/register')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_user():
     """Register a user."""
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/secret')
+
     return render_template('register.html', form=form)
+
+@app.route('/secret', methods=['GET'])
+def show_secret():
+    """Show the secret page."""
+    return render_template('secret.html')
