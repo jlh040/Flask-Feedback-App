eb3550bd40b3d0e4e5e9e09b0f12ba15f4994eaf
Jeff Hall <jeff.leon.hall@gmail.com>
2021-04-11 13:17:44 -0700

    created a register WTForm and added the base '/' route

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, session, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import User, connect_db
+from models import db, User, connect_db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///some_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///user_db'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -13,3 +13,8 @@ connect_db(app)
 app.config['SECRET_KEY'] = 'c8n1m9x3'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+@app.route('/', methods=['GET'])
+def redirect_route():
+    """Redirect user to /register."""
+    return redirect('/register')
